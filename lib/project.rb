7cb86c9ca9ace9766b978ba3551083d7007c1dbe
Peter Wells <petethebeatwells@gmail.com>
2020-01-10 10:03:14 -0800

    get first method for project passing (title)

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,17 @@
+require 'pry'
+
 class Project
 attr_reader :id
 attr_accessor :title
 
 def initialize(attributes)
-  title = attributes.fetch("title")
+  @title = attributes.fetch(:title)
+  @id = attributes.fetch(:id)
 end
 
-
+def title
+  @title
+end
 
 
 end
