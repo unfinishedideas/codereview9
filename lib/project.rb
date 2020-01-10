01c56ccc7f7b02a8a6cf11318d4f5ac2ab8ad624
Peter Wells <petethebeatwells@gmail.com>
2020-01-10 11:54:06 -0800

    add a volunteers function to return the list of volunteers for a project

--- lib/project.rb
+++ lib/project.rb
@@ -47,4 +47,16 @@ class Project
     DB.exec("DELETE FROM projects WHERE id = #{@id}")
   end
 
+  def volunteers
+    volunteer_query = DB.exec("SELECT * FROM volunteers WHERE project_id = #{@id}")
+    volunteers = []
+    volunteer_query.each do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id")
+      project_id = @id
+      volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
+    end
+    volunteers
+  end
+
 end
