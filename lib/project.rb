72abb5159c9dc32c0db9b862e7feef46ba99c64e
Peter Wells <petethebeatwells@gmail.com>
2020-01-10 10:19:23 -0800

    add a passing .all function to project

--- lib/project.rb
+++ lib/project.rb
@@ -1,21 +1,32 @@
 require 'pry'
 
 class Project
-attr_reader :id
-attr_accessor :title
+  attr_reader :id
+  attr_accessor :title
 
-def initialize(attributes)
-  @title = attributes.fetch(:title)
-  @id = attributes.fetch(:id)
-end
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
 
-def save
-  result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
-  @id = result.first().fetch("id").to_i
-end
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
 
-def ==(other_project)
-  self.title().downcase.eql?(other_project.title().downcase())
-end
+  def ==(other_project)
+    self.title().downcase.eql?(other_project.title().downcase())
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
 
 end
