f10fa84524b94e4e3577f6373556605926e687f2
Peter Wells <petethebeatwells@gmail.com>
2020-01-10 12:54:44 -0800

    add basic front-end functionality to add and view projects

--- app.rb
+++ app.rb
@@ -11,3 +11,16 @@ DB = PG.connect({:dbname => "volunteer_tracker"})
 get('/') do
   erb(:homepage)
 end
+
+post('/projects') do
+  title = params[:title]
+  @project = Project.new({:title => title, :id => nil})
+  @project.save
+  @projects = Project.all
+  erb(:view_projects)
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:view_projects)
+end
